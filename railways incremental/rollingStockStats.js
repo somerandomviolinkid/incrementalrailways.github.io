8a472b4233020154c1aa1a66a66a83d2cfbf0fda
mrbobbyguy <84521318+somerandomviolinkid@users.noreply.github.com>
2022-12-10 20:19:20 -0600

    Delete railways incremental directory

--- railways incremental/rollingStockStats.js
+++
@@ -1,1 +0,0 @@
-const rollingStockStats = { locomotiveStats: { d1000: { cost: 500000, topSpeed: 30, maxCars: 4, fuelCapacity: 1000, fuelEfficiency: 250 } } }
--- railways incremental/routes.js
+++
@@ -1,2 +0,0 @@
-let routeNames = []
-let routes = {}
