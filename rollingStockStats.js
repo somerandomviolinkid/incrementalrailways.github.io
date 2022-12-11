736f294eb50cd53319d3e5fc4687a92066562a8b
mrbobbyguy <84521318+somerandomviolinkid@users.noreply.github.com>
2022-12-11 10:15:44 -0600

    Update rollingStockStats.js

--- rollingStockStats.js
+++ rollingStockStats.js
@@ -1,1 +1,25 @@
-const rollingStockStats = { locomotiveStats: { d1000: { cost: 500000, topSpeed: 30, maxCars: 4, fuelCapacity: 1000, fuelEfficiency: 250 } } }
+const rollingStockStats = { 
+    locomotiveStats: { 
+        d1000: { 
+            cost: 500000, 
+            topSpeed: 30, 
+            maxCars: 4, 
+            fuelCapacity: 1000, 
+            fuelEfficiency: 250
+        },
+        d1010: { 
+            cost: 700000, 
+            topSpeed: 32, 
+            maxCars: 5, 
+            fuelCapacity: 1100, 
+            fuelEfficiency: 250 
+        },
+        d1020: { 
+            cost: 1100000, 
+            topSpeed: 35, 
+            maxCars: 6, 
+            fuelCapacity: 1250, 
+            fuelEfficiency: 260 
+        }
+    } 
+}
