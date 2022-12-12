7e58923901495d7fccc055ecdc45922d7eec116a
mrbobbyguy <84521318+somerandomviolinkid@users.noreply.github.com>
2022-12-11 20:01:43 -0600

    Update init.js

--- init.js
+++ init.js
@@ -3,5 +3,13 @@
 if (routeNames.length == 1) {
     document.getElementById("route1Button").style.display = "none";
     document.getElementById("routeInfo").style.display = "block";
+
+    if (routes[routeNames[routeNumber]].locomotives >= 1 && routes[routeNames[routeNumber]].freightCars >= 1 && routes[routeNames[routeNumber]].allocatedFuel >= 100) {
+        document.getElementById("routeControlDiv").style.display = "block";
+        document.getElementById("routeStep1").style.display = "inline";
+    } else {
+        document.getElementById("routeControlDiv").style.display = "none"; 
+    }
+
     updateRoutePage();
 }
