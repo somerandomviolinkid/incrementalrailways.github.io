ff7de8c5665939202a798d68812a06c8ffbe5a00
mrbobbyguy <84521318+somerandomviolinkid@users.noreply.github.com>
2022-12-11 20:02:54 -0600

    Update saving.js

--- saving.js
+++ saving.js
@@ -1,119 +1,57 @@
-body {
-    background-color: dimgrey;
-    margin: 0 0 0 0;
-    padding: 0 0 0 0;
+function saveData() {
+
+    //saves stuff to local storage
+    window.localStorage.setItem("saveKey", JSON.stringify({
+        data,
+        routeNames,
+        routes
+    }))
+
 }
 
-.mainDiv {
-    border-color: black;
-    border-style: solid;
-    border-radius: 5%;
-    border-width: 1px;
-    background-color: lightgray;
-    display: inline;
-    float: left;
-    padding: 8px;
-    width: 20%;
-    margin: 0 0 0 0;
+function loadData() {
+
+    //loads stuff from local storage
+    const saveString = localStorage.getItem("saveKey");
+    if (!saveString) return;
+    const save = JSON.parse(saveString);
+
+    data = save.data;
+    routeNames = save.routeNames;
+    routes = save.routes;
+    updateResources();
+
 }
 
-#settingsDiv {
-    width: 10%;
-    float: right;
-    background-color:aliceblue;
+function resetData() {
+    //do this if your workers go on strike
+    localStorage.clear();
+    location.reload();
 }
 
-.subDiv {
-    border-radius: 5%;
-    border-color: black;
-    border-style: solid;
-    width: 90%;
-    height: fit-content;
-    background-color: darkgray;
-    padding: 8px;
-    text-align: right;
-    margin: auto;
+//from henryL from an earlier project
+function downloadData() {
+    //exports data as json file
+    saveData();
+    const json = localStorage.getItem("saveKey");
+    const fileName = "save.json";
+    const a = document.createElement('a');
+    const type = fileName.split(".").pop();
+    a.href = URL.createObjectURL(new Blob([json], { type: `text/${type === "txt" ? "plain" : type}` }));
+    a.download = fileName;
+    a.click();
 }
 
-.stuffCount {
-    font-size: 22px;
-    font-weight: 4px;
-    font-family: 'Courier New', Courier, monospace;
+async function uploadData() {
+    //imports data from json file
+    try {
+        const [fileHandle] = await window.showOpenFilePicker();
+        const file = await fileHandle.getFile();
+        const contents = await file.text();
+        localStorage.setItem("saveKey", contents);
+        loadData();
+    } catch (err) { }
 }
 
-h2 {
-    text-align: center;
-    font-family: 'Courier New', Courier, monospace;
-    font-size: 36px;
-}
-
-h3 {
-    text-align: center;
-    font-family: 'Courier New', Courier, monospace;
-    font-size: 30px;
-}
-
-button {
-    border-radius: 10%;
-    padding: 8px;
-    background-color: lightsteelblue;
-    transition: 0.5s;
-    margin-left: 16px;
-    font-size: 24px;
-    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-    margin: auto;
-}
-
-button:hover {
-    background-color: steelblue;
-    cursor: pointer;
-}
-
-.routeInfoP {
-    font-size: 20px;
-    display: inline;
-    font-family: 'Courier New', Courier, monospace;
-    margin: 0;
-}
-
-.addRollingStockButton {
-    padding: 0;
-    float: right;
-    display: inline;
-    margin: 0;
-    width: 10%;
-    height: 80%;
-    text-align: center;
-}
-
-#routeInfo {
-    margin: auto;
-    display: none;
-    height: fit-content;
-    text-align: left;
-    padding: 12px;
-}
-
-.settingButton {
-    margin:auto;
-    width: 90%;
-    display: block;
-}
-
-.subRouteDiv {
-    width: 98%;
-    border-color: black;
-    border-style: solid;
-    border-radius: 5%;
-    background-color: cadetblue;
-    margin: auto;
-    height: fit-content;
-    padding: 4px;
-    height: 50px;
-}
-
-#routeControlButton {
-    width: 95%;
-    margin: auto;
-    padding: 5px;
-}
+loadData();
+setInterval(saveData, 15000);
