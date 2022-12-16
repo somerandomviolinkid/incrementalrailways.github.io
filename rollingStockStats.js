988dd068f192d676b56c2e0be5f9b82021d03f34
mrbobbyguy <84521318+somerandomviolinkid@users.noreply.github.com>
2022-12-15 20:19:34 -0600

    Update rollingStockStats.js

--- rollingStockStats.js
+++ rollingStockStats.js
@@ -1,7 +1,7 @@
-const rollingStockStats = { 
+let rollingStockStats = { 
     locomotiveStats: { 
         d1000: { 
-            cost: 500000, 
+            cost: 300000, 
             topSpeed: 30, 
             maxCars: 4, 
             fuelCapacity: 1000, 
@@ -9,7 +9,7 @@ const rollingStockStats = {
             weight: 200
         },
         d1010: { 
-            cost: 700000, 
+            cost: 650000, 
             topSpeed: 32, 
             maxCars: 5, 
             fuelCapacity: 1100, 
@@ -17,7 +17,7 @@ const rollingStockStats = {
             weight: 210
         },
         d1020: { 
-            cost: 1100000, 
+            cost: 1250000, 
             topSpeed: 35, 
             maxCars: 6, 
             fuelCapacity: 1250, 
