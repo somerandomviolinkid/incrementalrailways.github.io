4119597424a069e48e32f3f0e142b745ee39819c
mrbobbyguy <84521318+somerandomviolinkid@users.noreply.github.com>
2022-12-15 20:19:16 -0600

    Add files via upload

---
+++ research.js
@@ -0,0 +1,13 @@
+let researchedThings = []
+let research = {
+    dispatchingUpgrade1: {
+        cost: 10,
+    },
+    dispatchingUpgrade2: {
+        req,
+        cost: 250,
+    },
+    dispatchingUpgrade3: {
+        cost: 5000,
+    }
+}
