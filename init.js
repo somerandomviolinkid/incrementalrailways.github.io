360196dd328d51efec6806896cf02e11d2bc6564
mrbobbyguy <84521318+somerandomviolinkid@users.noreply.github.com>
2022-12-18 19:45:40 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,5 +1,3 @@
-nextCurrentButton();
-
 if (routeNames.length == 1) {
     document.getElementById("route1Button").style.display = "none";
     document.getElementById("routeInfo").style.display = "block";
@@ -16,4 +14,47 @@ if (routeNames.length == 1) {
     }
 
     updateRoutePage();
+}
+
+document.getElementById("trainTabButton").click();
+
+if (data.buildingStats.freightDepot.built == true) {
+    document.getElementById("buildFreightDepot").style.display = "none";
+    document.getElementById("freightProgress").style.display = "block";
+    moveFreightProgressBar();
+    updateFreightDepotInfo();
+} else {
+    document.getElementById("collectFeesButton").style.display = "none";
+    document.getElementById("freightDepotUpgradesDiv").style.display = "none";
+}
+
+//op code
+if (data.buildingStats.freightDepot.upgrades.speed1 == true) {
+    document.getElementById("upgradeFreightDepotSpeed1").style.display = "none";
+    document.getElementById("upgradeFreightDepotSize1").style.display = "block";
+} else {
+    document.getElementById("upgradeFreightDepotSize1").style.display = "none";
+}
+
+if (data.buildingStats.freightDepot.upgrades.size1 == true) {
+    document.getElementById("upgradeFreightDepotSize1").style.display = "none";
+    document.getElementById("upgradeFreightDepotTrack1").style.display = "block";
+} else {
+    document.getElementById("upgradeFreightDepotTrack1").style.display = "none";
+}
+
+if (data.buildingStats.freightDepot.upgrades.track1 == true) {
+    document.getElementById("upgradeFreightDepotTrack1").style.display = "none";
+}
+
+if (research.freightDepot1.researched == false) {
+    document.getElementById("upgradeFreightDepotSpeed2").style.display = "none";
+    document.getElementById("upgradeFreightDepotSpeed3").style.display = "none";
+    document.getElementById("upgradeFreightDepotSpeed4").style.display = "none";
+    document.getElementById("upgradeFreightDepotSize2").style.display = "none";
+    document.getElementById("upgradeFreightDepotSize3").style.display = "none";
+    document.getElementById("upgradeFreightDepotSize4").style.display = "none";
+    document.getElementById("upgradeFreightDepotTrack2").style.display = "none";
+    document.getElementById("upgradeFreightDepotTrack3").style.display = "none";
+    document.getElementById("upgradeFreightDepotTrack4").style.display = "none";
 }
