1246f6d71402ed41d7a3956bc42e8792857bc88d
mrbobbyguy <84521318+somerandomviolinkid@users.noreply.github.com>
2022-12-18 19:46:43 -0600

    Update research.js

--- research.js
+++ research.js
@@ -1,13 +1,6 @@
-let researchedThings = []
 let research = {
-    dispatchingUpgrade1: {
-        cost: 10,
-    },
-    dispatchingUpgrade2: {
-        req,
-        cost: 250,
-    },
-    dispatchingUpgrade3: {
-        cost: 5000,
+    freightDepot1: {
+        researched: false,
+        cost: 100
     }
 }
